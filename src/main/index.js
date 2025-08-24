b04f10a75e6bd024d70b1b1c5fedb7ea2df66d22
Brandon Rezko <35571303+brandon-rezko@users.noreply.github.com>
2025-08-24 22:45:13 +0800

    refactor(main): register web handlers and improve app lifecycle events

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/logo-solid.png?asset'
+import icon from '../../resources/logo-solid.png?asset' 
 import { initDB } from './db/index.js'
 import { registerHandler } from './service/index.js'
 import { init as initInterval } from './interval/interval.js'
