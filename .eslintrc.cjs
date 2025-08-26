eb3f3162dada151b1651e1a5eae7ea2de583b2f5
Brandon Rezko <35571303+brandon-rezko@users.noreply.github.com>
2025-08-26 12:06:29 +0800

    style(lint): set ECMAScript 2022 parsing with ES modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution') 
 
-module.exports = {
+module.exports = { 
   extends: [
     'eslint:recommended',
     // 'plugin:vue/vue3-recommended',
