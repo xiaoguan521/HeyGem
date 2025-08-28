1c39a37b5c9d8251db76309a39b0676feb150159
Brandon Rezko <35571303+brandon-rezko@users.noreply.github.com>
2025-08-28 21:19:18 +0800

    build(electron): optimize dependency handling with externalizeDepsPlugin

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { resolve } from 'path' 
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({   
